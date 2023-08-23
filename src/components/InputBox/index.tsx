9fc838c13776cc4fac56c1590d64d6b59ed10599
jiseon <ccc2887@naver.com>
2023-08-23 13:53:39 +0900

    2023.08.23. 13:50

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
-export default function index() {
+//            interface: Input 상자 컴포넌트 Props          //
+interface Props {
+label: string;
+type: 'text'|'password';
+placeholder: string;
+icon?: string;
+errorMessage?: string;
+onButtonClik?: () => void;
+}
+
+//           state: Properties         //
+export default function InputBox(props: Props) {
+  const{label, type, placeholder, icon, errorMessage} = props;
+  const{onButtonClik} = props;
+
+        //         render: Input 상자 렌더링           //
   return (
-    <div>index</div>
+    <div className= 'inputbox'>
+      <div className='inputbox-label'>{label}</div>
+      <div className={errorMessage ? 'inputbox-container-error': 'inputbox-container'}>
+       <input className='input'type={type} placeholder={placeholder}/>
+       {onButtonClik! == undefined && ( 
+       <div className='icon-button' onClick={onButtonClik}>
+         {icon}
+          <div className='right-arrow-icon'></div>
+        </div>
+      )}
+      </div>
+      <div className='inputbox-message'>{errorMessage}</div>
+    </div>
   )
 }
+
